de9544682764afb3b5e54276765bbc2481cc7f29
chengpu0422 <146065192+chengpu0422@users.noreply.github.com>
2023-10-31 15:07:22 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b;
+   scanf("%d%d",&a,&b);
+
+   if(a<=60)
+   {
+     int c;
+     c=a*b;
+     printf("%d\n",c);
+   }
+   if(a>60&&a<=120)
+   {
+    int d;
+    d=a-60;
+    double e,f;
+    e=(double)d*b*1.33;
+    f=(double)e+(60*b);
+    printf("%.1f",f);
+   }
+   if(a>120)
+   {
+    int g;
+    g=a-120;
+    double h,i;
+    h=(double)g*b*1.66;
+    i=(double)h+60*b*1.33+60*b;
+    printf("%.1f\n",i);
+   }
+}
 
